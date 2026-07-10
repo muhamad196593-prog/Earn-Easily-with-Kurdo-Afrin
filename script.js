666e1b9963d908c9e4fdc7b887445ac2caf3f2a7
muhamad196593-prog <muhamad196593@gmail.com>
2026-07-10 21:57:04 +0300

    Implement password visibility toggle function
    Added a function to toggle password visibility.

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ registerBtn.addEventListener("click", () => {
     // لاحقًا:
     // window.location.href = "register.html";
 });
+function togglePassword(){
+
+const password =
+document.getElementById("password");
+
+const icon =
+document.querySelector(".toggle-password");
+
+if(password.type==="password"){
+
+password.type="text";
+
+icon.classList.remove("fa-eye");
+icon.classList.add("fa-eye-slash");
+
+}else{
+
+password.type="password";
+
+icon.classList.remove("fa-eye-slash");
+icon.classList.add("fa-eye");
+
+}
+
+}
